4643abb079405390ce849e815e9e7171f9c83691
Danylo Onishchuk <danyloonishchuk@Danylos-MacBook-Pro-2.local>
2022-10-22 17:19:48 +0300

    fetch currency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
+import { FetchTaskModule } from './fetchTask/fetchTask.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'lucky.db.elephantsql.com',
-      port: 5432,
-      username: 'rcugtgzb',
-      password: '5YYmcMBtB9MeO9vrfluznWgSlRk_k5DQ',
-      database: 'rcugtgzb',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [],
       synchronize: true,
     }),
+    ScheduleModule.forRoot(),
+    FetchTaskModule,
   ],
 })
 export class AppModule {}
